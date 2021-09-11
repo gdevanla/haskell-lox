c1e6561b59f4ef186750841c15d777ea1a15fd02
grdvnl <grdvnl@gmail.com>
2021-09-11 14:59:32 -0700

    Create new Scanner module and add tests

--- test/test_lexer.hs
+++ test/test_lexer.hs
@@ -12,9 +12,63 @@ import Data.Array
 import System.FilePath
 import System.Directory (listDirectory)
 
+import Scanner
+import Data.Either
 
-test1 = testCase "test" $ do
-  1 @?= 1
+testToken test_name token_str expected_tok = testCase test_name $ do
+  let result = scanner token_str
+  case result of
+    Left x -> assertFailure $ show x
+    Right (x : _) -> assertEqual token_str (tokinfo_type x) expected_tok
+
+
+
+testInvalidToken test_name token_str = testCase test_name $ do
+  let result = scanner token_str
+  case result of
+    Left x -> return ()
+    Right x -> assertFailure $ show x
+
+testSingleCharToken = testToken "testSingleCharToken" "+" PLUS
+
+testDoubleCharToken = testToken "testDoubleCharToken"  "==" EQUAL_EQUAL
+
+testKeywordToken = testToken "testKeywordToken" "class" CLASS
+
+testScanDouble_1 = testToken "testScanDouble_1" "1121.1121" (NUMBER 1121.1121)
+testScanDouble_2 = testToken "testScanDouble_2" "0.1121" (NUMBER 0.1121)
+
+-- We don't like the Lexer doing this, but we will try handling these scenarios in the parser
+testScanDouble_3_bad = testToken "testScanDouble_3" ".1121" (DOT)
+testScanDouble_4_bad = testToken "testScanDouble_4" "1121.0" (NUMBER 1121.0)
+testScanInvalidIdentifier_bad = testToken "testScanIdentifier" "1and" (NUMBER 1.0)
+
+testScanDoubleInvalid_2 = testInvalidToken "testScanDouble_2" "1121."
+
+testScanIdentifier = testToken "testScanIdentifier" "and_1" (IDENTIFIER "and_1")
+
+-- invalid tokens
+
+testSingleCharInvalidToken = testInvalidToken "testSingleCharInvalidToken" "%"
+
+testDoubleCharInvalidToken = testInvalidToken "testDoubleCharInvalidToken" "%%"
+
+
+
 
 main = do
-  defaultMain $ testGroup "tokenizer_tests_example_1" [test1]
+  defaultMain $ testGroup "tokenizer_tests_example_1" [
+    testSingleCharToken,
+    testDoubleCharToken,
+    testKeywordToken,
+    testScanDouble_1,
+    testScanDouble_2,
+    testScanDouble_3_bad,
+    testScanDouble_4_bad,
+    testScanIdentifier,
+    -- invalid tokens
+    testSingleCharInvalidToken,
+    testDoubleCharInvalidToken,
+    testScanInvalidIdentifier_bad
+    ]
+  --defaultMain tests
