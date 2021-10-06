11adeff5d0fd0f70527847b785320c6cdee8fe6f
grdvnl <grdvnl@gmail.com>
2021-10-06 10:29:12 -0700

    Code cleanup

--- app/scanner.hs
+++ app/scanner.hs
@@ -1,35 +1,35 @@
-{-# LANGUAGE OverloadedStrings#-}
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
-import Import hiding (many, (<|>), try)
-import System.Environment
-import System.IO (print)
-import RIO.ByteString as BS
-import Data.Text.Encoding
-import Run
-import RIO.Process
+{-# LANGUAGE NoImplicitPrelude #-}
 
 --import Options.Applicative.Simple
-import qualified Paths_haskell_lox
 
-import Data.Text as T
 import Data.Char
-import FunctionsAndTypesForParsing (parseWithEof, parseWithLeftOver, regularParse)
-import Text.Parsec.String as PS
-import Text.Parsec.Char as PC
+import Data.Text as T
+import Data.Text.Encoding
 -- import Text.Parsec.String.Char
 -- import Text.Parsec.String.Char (anyCh
-import Text.Parsec.String.Combinator (many1)
 
-import Text.Parsec
-import RIO.Partial (read)
 import ExprInterpreter
+import FunctionsAndTypesForParsing (parseWithEof, parseWithLeftOver, regularParse)
+import Import hiding (many, try, (<|>))
+import qualified Paths_haskell_lox
+import RIO.ByteString as BS
+import RIO.Partial (read)
+import RIO.Process
+import Run
+import System.Environment
+import System.IO (print)
+import Text.Parsec
+import Text.Parsec.Char as PC
+import Text.Parsec.String as PS
+import Text.Parsec.String.Combinator (many1)
 
 main :: IO ()
 main = runSimpleApp $ do
   args <- liftIO getArgs
   case args of
-    filename:_ -> do
+    filename : _ -> do
       contents <- decodeUtf8 <$> BS.readFile filename
       void $ liftIO $ runScript contents
     [] -> do
