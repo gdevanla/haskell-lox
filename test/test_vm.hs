1a3aa735256e1afc726d838684a1d4a39d1a7f50
grdvnl <grdvnl@gmail.com>
2021-10-12 22:38:19 -0700

    Improve tests

--- test/test_vm.hs
+++ test/test_vm.hs
@@ -21,15 +21,14 @@ import CloxByteCode
 import CloxByteCodeGen
 import CloxInterpreter
 
-
 import Data.Sequence
 
 test_compiler input expected = testCase input $ do
   opcodes' <- compileToByteCode . T.pack $ input
   let opcodes = fromRight [] $ opcodes'
   vm <- runInterpreter [Chunk (Seq.fromList opcodes)]
-  let actual_stack = stack vm
-  assertEqual "" expected actual_stack
+  -- let actual_stack = stack vm
+  assertEqual "" (M.fromList expected) (globals vm)
 
 test_locals = testCase "test_locals" $ do
   let code = "var result1; var result2; var result3; var x1=200; {var x=10; {result1=x1+x; x=15; result2=x1+x;} result3=x;}"
@@ -48,50 +47,48 @@ test_locals = testCase "test_locals" $ do
 
 testData = let
   expected = [
-    [DValue 10.0],
-    [DValue 9.0],
-    [DValue 4.0],
-    [DValue 8.0],
-    [DValue 28.0],
-    [DValue 22.0],
-    [DValue 1.0],
-    [BValue True],
-    [BValue True],
-    [BValue True],
-    [BValue True],
-    [BValue False],
-    [BValue True],
-    [BValue True],
-    [SValue "test_var"],
-    [], -- print statement, nothing on stack
-    [], -- global definition
-    [], -- var and print
-    [DValue (-20.0)], -- var and print
-    [DValue (-40.0)], -- var and print
-    []
+    [("x", DValue 10.0)],
+    [("x", DValue 9.0)],
+    [("x", DValue 4.0)],
+    [("x", DValue 8.0)],
+    [("x", DValue 28.0)],
+    [("x", DValue 22.0)],
+    [("x", DValue 1.0)],
+    [("x", BValue True)],
+    [("x", BValue True)],
+    [("x", BValue True)],
+    [("x", BValue True)],
+    [("x", BValue False)],
+    [("x", BValue True)],
+    [("x", BValue True)],
+    [("x", SValue "test_var")],
+    [],
+    [("x", SValue "print this")], -- print statement, nothing on stack
+    [("x", DValue (-20.0))], -- var and print
+    [("x", DValue (-40.0))], -- var and print
+    [("x", DValue (-10)), ("y", DValue (-20))]
     ]
   expressions = [
-      "1+2+3+4;",
-      "10-2+1;",
-      "10-5-1;",
-      "10+2*3-8;",
-      "(10+2)*3-8;",
-      "10*2+4/2;",
-      "10/2-4;",
-      "true;",
-      "1<2;",
-      "2>1;",
-      "5==5;",
-      "5!=5;",
-      "5<=5;",
-      "5>=5;",
-      "\"test_var\";",
+      "var x=1+2+3+4;",
+      "var x=10-2+1;",
+      "var x=10-5-1;",
+      "var x=10+2*3-8;",
+      "var x=(10+2)*3-8;",
+      "var x=10*2+4/2;",
+      "var x=10/2-4;",
+      "var x=true;",
+      "var x=1<2;",
+      "var x=2>1;",
+      "var x=5==5;",
+      "var x=5!=5;",
+      "var x=5<=5;",
+      "var x=5>=5;",
+      "var x=\"test_var\";",
       "print 10000+20000;",
-      "var x = 100;", -- TODO: Add test to check the global hash table
       "var x = \"print this\";print x;", -- TODO: Add test to check the global hash table
-      "var x=-10;x+x;", -- TODO: Add test to check the global hash table
-      "var x=-10;x=-20;x+x;", -- TODO: Add test to check the global hash table
-      "var x=-10;y=-20;" -- TODO: Check Interpreter return value for RunTimeError
+      "var x=-10;x=x+x;", -- TODO: Add test to check the global hash table
+      "var x=-10;x=-20;x=x+x;", -- TODO: Add test to check the global hash table
+      "var x=-10;var y=-20;" -- TODO: Check Interpreter return value for RunTimeError
     ]
   in
   L.zip expressions expected
